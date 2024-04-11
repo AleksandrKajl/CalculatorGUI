a330808d5b9aa974a903600e4fe4b0dfc04584a1
Aleksandr <kajlaleksandr1986@gmail.com>
2024-04-11 19:15:09 +0700

    Найден баг на повторном нажатии нуля после цифры.

--- CalculatorGUI/main.cpp
+++ CalculatorGUI/main.cpp
@@ -1,6 +1,5 @@
-#include"calculator.h"
-#include"input.h"
-
+#include "input.h"
+#include "calculator.h"
 
 int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, int nCmdShow)
 {
@@ -11,13 +10,13 @@ int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, 
 
 BOOL CALLBACK DlgProc(HWND hWnd, UINT uMsg,WPARAM wParam, LPARAM lParam)
 {
-	static Input inp;
-	static Calculator calc;
+	static Input input;
+	static Calculator calculator;
 	switch (uMsg)
 	{
 	case WM_INITDIALOG:
-		inp.hEdit = GetDlgItem(hWnd, IDC_MYEDIT);
-		inp.setBuf('0');
+		input.hEdit = GetDlgItem(hWnd, IDC_MYEDIT);
+		input.setBuf('0');
 		break;
 
 	case WM_COMMAND:
@@ -26,98 +25,99 @@ BOOL CALLBACK DlgProc(HWND hWnd, UINT uMsg,WPARAM wParam, LPARAM lParam)
 //=====================Кнопки от 1 до 9========================================
 			{
 		case IDC_ONE:
-			inp.setBuf('1');
+			input.setBuf('1');
 			break;
 		case IDC_TWO:
-			inp.setBuf('2');
+			input.setBuf('2');
 			break;
 		case IDC_TREE:
-			inp.setBuf('3');
+			input.setBuf('3');
 			break;
 		case IDC_FOUR:
-			inp.setBuf('4');
+			input.setBuf('4');
 			break;		
 		case IDC_FIVE:	
-			inp.setBuf('5');
+			input.setBuf('5');
 			break;		
 		case IDC_SIX:	
-			inp.setBuf('6');
+			input.setBuf('6');
 			break;		
 		case IDC_SEVEN:	
-			inp.setBuf('7');
+			input.setBuf('7');
 			break;		
 		case IDC_EIGHT:	
-			inp.setBuf('8');
+			input.setBuf('8');
 			break;		
 		case IDC_NINE:	
-			inp.setBuf('9');
+			input.setBuf('9');
 			break;		
 		case IDC_ZERO:
-			if (inp.idx > inp.idxSign+1 && inp.input[inp.idxSign + 1] == '0' && inp.input[inp.idxSign + 2] != ',')
+			//!Баг с повторным нажатием нуля после цифры
+			if (input.idx > input.idxSign+1 && input.input[input.idxSign + 1] == '0' && input.input[input.idxSign + 2] != ',')
 				break;
-			inp.setBuf('0');
+			input.setBuf('0');
 			break;
 			}
 //=============================================================================
 //=====================Кнопки <--, +, -, /, *, =, ',', +/-, C================== 
 			{
 		case IDC_BACKSPACE:
-			if(inp.idx > 1)
-				inp.idx--;
+			if(input.idx > 1)
+				input.idx--;
 			else
-				inp.input[0] = '0';
+				input.input[0] = '0';
 
-			inp.input[inp.idx] = '\0';
-			SendMessage(inp.hEdit, WM_SETTEXT, 0, LPARAM(inp.input));
+			input.input[input.idx] = '\0';
+			SendMessage(input.hEdit, WM_SETTEXT, 0, LPARAM(input.input));
 			break;
 		case IDC_PLUS:
-			inp.setSign('+');
+			input.setSign('+');
 			break;
 		case IDC_MINUS:
-			inp.setSign('-');
+			input.setSign('-');
 			break;
 		case IDC_MUL:
-			inp.setSign('*');
+			input.setSign('*');
 			break;
 		case IDC_DIV:
-			inp.setSign('/');
+			input.setSign('/');
 			break;
 		case IDC_POINT:
-			if (!inp.checkPoint(inp.idx))
-				inp.setSign(',');
+			if (!input.checkPoint(input.idx))
+				input.setSign(',');
 			break;		   
 		case IDC_SIGN:	   
-			inp.doSignVal('-');
+			input.doSignVal('-');
 			break;
 		case IDC_EQ:
 			//Отправляет сообщение окну, минуя очередь
-			inp.idx = SendMessage(inp.hEdit, WM_GETTEXT, 256, (LPARAM)inp.input);
-			if (inp.checkInput())
-				inp.idx = calc.doColculations(inp.input);
+			input.idx = SendMessage(input.hEdit, WM_GETTEXT, 256, (LPARAM)input.input);
+			if (input.checkInput())
+				input.idx = calculator.doColculations(input.input);
 			else
-				strcpy(inp.input,"Invalid input");
-			SendMessage(inp.hEdit, WM_SETTEXT, 0, LPARAM(inp.input));
+				strcpy(input.input,"Invalid input");
+			SendMessage(input.hEdit, WM_SETTEXT, 0, LPARAM(input.input));
 			break;
 		case IDC_NULL:
-			inp.idxSign = 0;
-			inp.idx = 1;
-			inp.input[0] = '0';
-			inp.input[1] = '\0';
-			SendMessage(inp.hEdit, WM_SETTEXT, 0, LPARAM(inp.input));
+			input.idxSign = 0;
+			input.idx = 1;
+			input.input[0] = '0';
+			input.input[1] = '\0';
+			SendMessage(input.hEdit, WM_SETTEXT, 0, LPARAM(input.input));
 			break;
 			}
 //=============================================================================
 //=====================Кнопки x2, 2Vx, mod, % ===============================================
 		case IDC_EXP:
-			inp.setSign('^');
+			input.setSign('^');
 			break;
 
 		case IDC_SQRT:
-			inp.doSignVal('V');
+			input.doSignVal('V');
 			break;
 
 		case IDC_MOD:
-			inp.setSign('%');
+			input.setSign('%');
 			break;
 
 		case IDC_PER:
